7d7064f11e09c8de9ad13b080c4e6ccb0b07e8ec
Sandor Miskey <s@ndor.miskey.net>
2023-10-24 11:07:08 +0000

    cdc broker

---
+++ scripts/broker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Copyright TE-FOOD International GmbH., All Rights Reserved
+#
+
+# region: load config
+
+[[ ${WMS_PATH_RC:-"unset"} == "unset" ]] && WMS_PATH_RC=${WMS_PATH_BASE}/scripts/common.sh
+if [ ! -f  $WMS_PATH_RC ]; then
+	echo "=> WMS_PATH_RC ($WMS_PATH_RC) not found, make sure proper path is set or you execute this from the repo's 'scrips' directory!"
+	exit 1
+fi
+source $WMS_PATH_RC
+
+if [[ ${WMS_PATH_SCRIPTS:-"unset"} == "unset" ]]; then
+	commonVerify 1 "WMS_PATH_SCRIPTS is unset"
+fi
+commonPP $WMS_STACK4_S2_SRC
+
+# endregion: config
+
+cd $WMS_STACK4_S2_SRC
+go mod tidy
+go run ./...
